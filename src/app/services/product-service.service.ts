99c28b722ee397c162a8fb2098e1101ad11a3e26
Rociliu <mrrosa.carrasco@gmail.com>
2023-02-20 10:26:12 +0100

    creados array y funciones para añadir productos desde el formulario y de la api

--- src/app/services/product-service.service.ts
+++ src/app/services/product-service.service.ts
@@ -1,8 +1,9 @@
 /* Importamos el HttpClient */
 import { HttpClient } from '@angular/common/http';
 
-/* Importamos la interfaz Observable */
+/* Importamos la interfaz Product */
 import { Product } from '../interfaces/interface';
+
 
 /* Importamos el Injectable */
 import { Injectable } from '@angular/core';
@@ -20,10 +21,27 @@ const productsUrl = 'https://my-json-server.typicode.com/franlindebl/shopeame-ap
 
 export class ProductServiceService {
 
+  /* Creamos store que será un array tipo Product donde se irán añadiendo los productos*/
+  store: Product[] = [];
+
   constructor(private http: HttpClient) { }
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(productsUrl);
+  }
+
+  
+   /* Creamos una función que añade los resultados de la API a store*/
+  storeProducts(){
+    this.getProducts().subscribe(response => {
+      this.store = response;
+    })
+  }
+
+  
+  /* Creamos una función que añade un producto de tipo Product (nuestro objeto) a store*/
+  addProduct(product: Product) {
+    this.store.push(product);
   }
 }
 
