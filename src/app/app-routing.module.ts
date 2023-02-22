b636ae8d8c83872b5def1f7b74db3d951a356914
ana caballero <anacaballerof@hotmail.com>
2023-02-22 10:32:15 +0100

    arraglo botón mostrar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,8 +10,9 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
   { path: 'gestion', loadChildren: () => import('./modules/gestion/gestion.module').then(m => m.GestionModule) },
-  
-  { path: 'products', loadChildren: () => import('./modules/products/products.module').then(m => m.ProductsModule) }
+  { path: 'products/:id', loadChildren: () => import('./modules/product/product.module').then(m => m.ProductModule) },
+  // { path: "products/:id", component: ProductComponent},
+  { path: 'products', loadChildren: () => import('./modules/products/products.module').then(m => m.ProductsModule)}
 ];
 
 
